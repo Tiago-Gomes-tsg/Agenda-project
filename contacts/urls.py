e9a5f77998ab0161758271e676ecd7977e61d5cc
Tiago Gomes <tiagogomes71490@gmail.com>
2025-07-13 22:57:55 -0300

    First-Template

--- contacts/urls.py
+++ contacts/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path, include
+from django.urls import path
 from contacts.views import contact_views
 
 
 app_name = 'contacts'
 
 urlpatterns = [
-
+    path('', contact_views.index, name='contact'),
+    path('contact/', contact_views.index, name='contact'),
+    path('contacts/', contact_views.index, name='contact'),
 ]
---
+++ contacts/views/contact_views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from contacts.models import Contact
+
+
+def index(request):
+    contacts = Contact.objects.all()
+    
+    context = {
+        'contacts': contacts,
+    }
+    
+    return render(request, 'contacts/index.html', context)
